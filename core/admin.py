71b89d37446a6e6b6f163c3a452696bb032339f1
Lucas Miranda <lucas.miranda_est@compasso.com.br>
2019-12-13 16:49:21 -0300

    Adicionado as tabelas de evento, e usuarios apontados para utilizacao das mesmas pela pagina de admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Evento
+
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data', 'data_criacao')
+    list_filter = ('titulo', 'usuario',)
+
+
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Evento(models.Model):
+
+    titulo = models.CharField(max_length=255)
+    descricao = models.TextField(blank=True, null=True)
+    data = models.DateTimeField(verbose_name='Data do evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
+
